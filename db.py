c09c3fbb4adeefb1836d6be636f1211b7604ef04
Sandro Smarzaro <sansmarzaro@gmail.com>
2022-12-01 16:19:41 -0300

    [ADDED] orm mapping

---
+++ db.py
@@ -0,0 +1,69 @@
+from sqlalchemy import create_engine, Column, INTEGER, VARCHAR, DATE, NUMERIC, ForeignKey, DATETIME, BOOLEAN
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DIALECT = "postgresql"
+DRIVER = "psycopg2"
+USERNAME = "postgres"
+PASSWORD = "postgres"
+HOST = "localhost"
+PORT = "5432"
+DATABASE = "sales-db"
+
+engine = create_engine(
+    f"{DIALECT}+{DRIVER}://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
+)
+
+Base = declarative_base()
+
+
+class Client(Base):
+    __tablename__ = "client-tb"
+    id = Column(INTEGER, primary_key=True)
+    name = Column(VARCHAR(length=255), nullable=False)
+    birth_date = Column(DATE, nullable=False)
+
+    def __repr__(self):
+        return f"Client(id={self.id}, name={self.name}, birth_date={self.birth_date})"
+
+
+class Product(Base):
+    __tablename__ = "product-tb"
+    id = Column(INTEGER, primary_key=True)
+    name = Column(VARCHAR(length=255), nullable=False)
+    price = Column(NUMERIC(precision=8, scale=2), nullable=False)
+    inventory = Column(INTEGER, default=0, nullable=False)
+
+    def __repr__(self):
+        return f"Product(id={self.id}, name={self.name}, price={self.price}, inventory={self.inventory})"
+
+
+class ShoppingCart(Base):
+    __tablename__ = "shopping-cart-tb"
+    id = Column(INTEGER, primary_key=True)
+    sale_id = Column(INTEGER, ForeignKey("sale-tb.id"), nullable=False)
+    product_id = Column(INTEGER, ForeignKey("product-tb.id"), nullable=False)
+    quantity = Column(INTEGER, default=1, nullable=False)
+    sub_total_price = Column(NUMERIC(precision=8, scale=2))
+
+    def __repr__(self):
+        return f"ShoppingCart(id={self.id}, client_id={self.client_id}, product_id={self.product_id}, " \
+               f"quantity={self.quantity} sub_total_price={self.sub_total_price})"
+
+
+class Sale(Base):
+    __tablename__ = "sale-tb"
+    id = Column(INTEGER, primary_key=True)
+    client_id = Column(INTEGER, ForeignKey("client-tb.id"), nullable=False)
+    sale_date = Column(DATETIME, nullable=False)
+    total_price = Column(NUMERIC(precision=8, scale=2))
+    is_paid = Column(BOOLEAN, default=False, nullable=False)
+
+    def __repr__(self):
+        return f"Sale(id={self.id}, client_id={self.client_id},  sale_date={self.sale_date}, " \
+               f"total_price={self.total_price}, is_paid={self.is_paid})"
+
+
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()

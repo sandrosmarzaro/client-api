ceffe5a6ff385bb03e8d4a409c6292be4769ecf6
Sandro Smarzaro <sansmarzaro@gmail.com>
2022-12-02 19:11:58 -0300

    [ADDED] product blueprint

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 
 from blueprints.client import client_bp
+from blueprints.product import product_bp
 
 app = Flask(__name__)
 
 app.register_blueprint(client_bp, url_prefix='/api/v1/client')
+app.register_blueprint(product_bp, url_prefix='/api/v1/product')
 
 
 @app.route('/')
---
+++ blueprints/product.py
@@ -0,0 +1,72 @@
+from flask import Blueprint, jsonify, request
+from sqlalchemy.exc import NoResultFound
+
+from db import session, Product
+
+product_bp = Blueprint('product', __name__)
+
+@product_bp.route('/', methods=['GET', 'POST'])
+def products():
+    if request.method == 'GET':
+        return jsonify([product.to_dict() for product in session.query(Product).all()]), 200
+    elif request.method == 'POST':
+        product = request.get_json()
+        session.add(Product(
+            name=product['name'],
+            price=product['price']
+        ))
+        session.commit()
+        return jsonify(product), 201
+
+@product_bp.route('/<int:product_id>', methods=['GET', 'PATCH', 'PUT', 'DELETE'])
+def product(product_id):
+    if request.method == 'GET':
+        try:
+            product = session.query(Product).filter(Product.id == product_id).one()
+            return jsonify(product.to_dict()), 200
+        except NoResultFound:
+            return jsonify({
+                'message': 'Product not found'
+            }), 404
+
+    elif request.method == 'PUT':
+        product = request.get_json()
+        try:
+            query = session.query(Product).filter(Product.id == product_id)
+            query.update({
+                'name': product['name'],
+                'price': product['price'],
+                'inventory': product['inventory']
+            })
+            product = query.one().to_dict()
+            session.commit()
+            return jsonify(product), 200
+        except NoResultFound:
+            return jsonify({
+                'message': 'Product not found'
+            }), 404
+
+    elif request.method == 'PATCH':
+        product = request.get_json()
+        try:
+            query = session.query(Product).filter(Product.id == product_id)
+            query.update(product)
+            product = query.one().to_dict()
+            session.commit()
+            return jsonify(product), 200
+        except NoResultFound:
+            return jsonify({
+                'message': 'Product not found'
+            }), 404
+
+    elif request.method == 'DELETE':
+        try:
+            query = session.query(Product).filter(Product.id == product_id)
+            query.delete()
+            product = query.one().to_dict()
+            session.commit()
+            return jsonify(product), 200
+        except NoResultFound:
+            return jsonify({
+                'message': 'Product not found'
+            }), 404

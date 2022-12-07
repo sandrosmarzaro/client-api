cfb1a2a2880d0773643c5923d8d00bfbbffa1468
Sandro Smarzaro <sansmarzaro@gmail.com>
2022-12-07 12:46:45 -0300

    [FIXED] missing integrity error

--- blueprints/sale.py
+++ blueprints/sale.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from sqlalchemy.exc import NoResultFound
+from sqlalchemy.exc import NoResultFound, IntegrityError
 from db import session, Sale
 from datetime import datetime
 
@@ -12,14 +12,20 @@ def sales():
         return jsonify([sale.to_dict() for sale in session.query(Sale).all()]), 200
     elif request.method == 'POST':
         sale = request.get_json()
-        session.add(Sale(
-            client_id=sale['client_id'],
-            sale_date=datetime.now(),
-            total_price=0,
-            is_paid=False
-        ))
-        session.commit()
-        return jsonify(sale), 201
+        try:
+            session.add(Sale(
+                client_id=sale['client_id'],
+                sale_date=datetime.now(),
+                total_price=0,
+                is_paid=False
+            ))
+            session.commit()
+            return jsonify(sale), 201
+        except IntegrityError:
+            session.rollback()
+            return jsonify({
+                'message': 'Client not found'
+            }), 404
 
 
 @sale_bp.route('/<int:sale_id>', methods=['GET', 'PATCH', 'PUT', 'DELETE'])
@@ -68,10 +74,16 @@ def sale(sale_id):
         try:
             query = session.query(Sale).filter(Sale.id == sale_id)
             sale = query.one().to_dict()
-            session.delete(query)
+            query.delete()
             session.commit()
             return jsonify(sale), 200
         except NoResultFound:
+            session.rollback()
             return jsonify({
                 'message': 'Sale not found'
             }), 404
+        except IntegrityError:
+            session.rollback()
+            return jsonify({
+                'message': 'Sale contains products'
+            }), 400
